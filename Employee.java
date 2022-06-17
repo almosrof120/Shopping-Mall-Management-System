cbffdfdc3bfb450ab4631da7f29fb939af7d592c
Al Mosrof Mia <65063391+khan457@users.noreply.github.com>
2022-06-17 11:50:16 -0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,22 @@
+
+public class Employee {
+    String name;
+    String id;
+   String post;
+   
+  //constructor
+   
+  Employee(String n,String i,String p){
+   
+      name=n;
+      id=i;
+      post=p;
+   
+  }
+  
+ 
+public void takeClearance(){
+    System.out.println("Got clearance");
+    
+} 
+}
